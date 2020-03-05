680bc0a0c8f92c51aef0e40c8961fe8bd3af10b3
unknown <wangke@inv.com>
2020-03-05 17:07:59 +0800

    小程序初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,46 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    name: "Raylay",
+    age: 29,
+    students: [{
+        id: 1,
+        name: "张三",
+        age: 33
+      },
+      {
+        id: 2,
+        name: "李四",
+        age: 13
+      },
+      {
+        id: 3,
+        name: "王五",
+        age: 22
+      },
+      {
+        id: 4,
+        name: "赵六",
+        age: 21
+      },
+    ],
+    counter: 0,
   },
+  onClickAdd() {
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    //错误写法，这样不能刷新UI
+    // this.data.counter += 1
+    // console.log(this.data.counter)
 
+    // 正确写法 this.setData
+    this.setData({
+      counter: this.data.counter + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onClickSub() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
   }
+
 })
