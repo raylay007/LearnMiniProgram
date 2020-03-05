1c2c28015f4291ddb423b72bdc5597d8e27a14c8
unknown <wangke@inv.com>
2020-03-05 19:18:36 +0800

    03_小程序的双线程模型

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,78 @@ Page({
    * 页面的初始数据
    */
   data: {
+    msg: "哈哈"
+  },
+  onClick1() {
+    this.setData({
 
+      msg: "呵呵"
+    })
+
+    console.log(this.data.msg)
+  },
+  onClick2() {
+    this.setData({
+
+      msg: "嘿嘿"
+    })
+
+    console.log(this.data.msg)
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
